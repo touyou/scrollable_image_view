0d7e9e02ba292622e2fb3d692df2b0201b53ec08
touyou <fujiyou1121@gmail.com>
2019-10-31 19:06:16 +0900

    implement basic feature

--- lib/scrollable_image_view.dart
+++ lib/scrollable_image_view.dart
@@ -3,7 +3,18 @@ library scrollable_image_view;
 import 'package:flutter/material.dart';
 
 class ScrollableImageView extends StatefulWidget {
-  ScrollableImageView({Key key}) : super(key: key);
+  ScrollableImageView({
+    Key key,
+    @required this.imagePath,
+    @required this.viewSize,
+    this.imageSize,
+    this.imageScale,
+  }) : super(key: key);
+
+  final String imagePath;
+  final Size imageSize;
+  final Size viewSize;
+  final double imageScale;
 
   @override
   _ScrollableImageViewState createState() => _ScrollableImageViewState();
@@ -13,23 +24,36 @@ class _ScrollableImageViewState extends State<ScrollableImageView> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
-      width: 200,
+      height: widget.viewSize.height,
+      width: widget.viewSize.width,
       child: Scrollbar(
         child: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Scrollbar(
             child: SingleChildScrollView(
               scrollDirection: Axis.vertical,
-              child: Container(
-                color: const Color(0xff808000),
-                height: 300,
-                width: 300,
-              ),
+              child: getScaledImage(widget.imageScale, widget.imageSize),
             ),
           ),
         ),
       ),
     );
+  }
+
+  Image getScaledImage(double scale, Size size) {
+    if (scale != null) {
+      return Image.asset(widget.imagePath, scale: scale);
+    } else if (size != null) {
+      return Image.asset(
+        widget.imagePath,
+        width: widget.imageSize.width,
+        height: widget.imageSize.height,
+      );
+    } else {
+      return Image.asset(
+        widget.imagePath,
+        scale: 1.0,
+      );
+    }
   }
 }
